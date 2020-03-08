e30a72c613bb1ec077bde2d32efea26e03fd2516
HKlee057 <45294682+HKlee057@users.noreply.github.com>
2020-03-08 15:14:51 -0600

    Inicio de Slave con Sensor de Movimiento
    Se agregó código base del sensor de interferencia para lectura de sensor de movimiento.

---
+++ Project_MovSlave.X/build/default/production/Sensor_Movimiento.p1.d
@@ -0,0 +1,4 @@
+build/default/production/Sensor_Movimiento.p1:  \
+Sensor_Movimiento.c  \
+I2C.h  \
+Oscilador.h 
---
+++ Project_MovSlave.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Project_MovSlave.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Project_MovSlave.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=projectmovslave.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/projectmovslave.x.tar
